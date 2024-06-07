16e2d5f4ba4066870ede7151c1cf609ffa8958f8
Александр <LogvX_ba66@student.itstep.org>
2024-06-07 18:11:25 +0300

    initial commit

---
+++ ConsoleApplication2/ConsoleApplication2.cpp
@@ -0,0 +1,14 @@
+
+
+#include <iostream>
+
+int print(int* arr, int size) {
+	for (int i = 0; i < size; i++)
+	{
+		std::cout << arr[i] << "";
+	}
+}
+int main()
+{
+    std::cout << "Hello World!\n";
+}
